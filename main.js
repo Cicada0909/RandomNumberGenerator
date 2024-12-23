855d18b5054451b0a411608529db95645a7675ed
Alibek Tauirbaev <featternotving@gmail.com>
2024-12-23 07:16:37 +0500

    add Javascript

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
 const buttonRandom = document.querySelector(".button-random");
+const getRandomNumber = document.querySelector(".random-number");
+const loader = document.querySelector(".loader");
 
 function randomNumber() {
     const getMinNumberUser = document.querySelector(".mix-user");
     const userNumberMin = parseInt(getMinNumberUser.value);
     const getMaxNumberUser = document.querySelector(".max-user");
     const userNumberMax = parseInt(getMaxNumberUser.value);
-    const randomNumber = Math.floor(Math.random() * (userNumberMax - userNumberMin + 1)) + userNumberMin;
-    console.log(randomNumber);
-    
+    const userRandomNumber = Math.floor(Math.random() * (userNumberMax - userNumberMin + 1)) + userNumberMin;
+    return userRandomNumber
 }
 
-buttonRandom.addEventListener("click", (event) => randomNumber());
+function updateNumber() {
+    getRandomNumber.style.display= "none";
+    loader.style.display = "block";
+    setTimeout(() => {
+        loader.style.display = "none";
+        const generateRandomNumber = randomNumber();
+        if (isNaN(generateRandomNumber)) {
+            getRandomNumber.textContent = "Введите значения!";
+            getRandomNumber.style.display = "block";
+            getRandomNumber.style.fontWeight = "400";
+            return
+        }
+        getRandomNumber.style.fontWeight = "600";
+        getRandomNumber.textContent = randomNumber();
+        getRandomNumber.style.display = "block";
+    }, 1000);
+}
+
+buttonRandom.addEventListener("click", (event) => updateNumber());
